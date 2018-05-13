a1ce1d49fe17c56f85fb1481b8b810222a80c344
Magnus <magnumizer@gmail.com>
2018-05-13 04:34:53 +0200

    upload

---
+++ Mandatory1/ViewController.swift
@@ -0,0 +1,117 @@
+//
+//  ViewController.swift
+//  Mandatory1
+//
+//  Created by Magnus Holm Svendsen on 01/05/2018.
+//  Copyright © 2018 Magnus Holm Svendsen. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
+
+    @IBOutlet weak var textGhost: UITextField!
+    @IBOutlet weak var imageTitle: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    var newMedia: Bool?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textGhost.delegate = self
+        textGhost.isHidden = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func editLabel(_ sender: Any) {
+        textGhost.becomeFirstResponder()
+        textGhost.isHidden = false
+        textGhost.text = imageTitle.text
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if let name = textGhost.text {
+            imageTitle.text = name
+        }
+        hideEditing()
+        return false
+    }
+    
+    func hideEditing() {
+        textGhost.resignFirstResponder()
+        view.endEditing(true)
+        textGhost.isHidden = true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        hideEditing()
+    }
+    
+    @IBAction func useCamera(_ sender: AnyObject) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(
+            UIImagePickerControllerSourceType.camera) {
+            
+            let imagePicker = UIImagePickerController()
+            
+            imagePicker.delegate = self
+            imagePicker.sourceType =
+                UIImagePickerControllerSourceType.camera
+            imagePicker.mediaTypes = [kUTTypeImage as String]
+            imagePicker.allowsEditing = false
+            
+            self.present(imagePicker, animated: true,
+                         completion: nil)
+            newMedia = true
+        } else {
+            imageTitle.text = "Unable to access camera"
+            
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        let mediaType = info[UIImagePickerControllerMediaType] as! NSString
+        
+        self.dismiss(animated: true, completion: nil)
+        
+        if mediaType.isEqual(to: kUTTypeImage as String) {
+            let image = info[UIImagePickerControllerOriginalImage]
+                as! UIImage
+            
+            imageView.image = image
+            
+            if (newMedia == true) {
+                UIImageWriteToSavedPhotosAlbum(image, self, #selector(ViewController.image(image:didFinishSavingWithError:contextInfo:)), nil)
+            } else if mediaType.isEqual(to: kUTTypeMovie as String) {
+                // Code to support video here
+            }
+            
+        }
+    }
+    
+    @objc func image(image: UIImage, didFinishSavingWithError error: NSErrorPointer, contextInfo:UnsafeRawPointer) {
+        
+        if error != nil {
+            let alert = UIAlertController(title: "Save Failed",
+                                          message: "Failed to save image",
+                                          preferredStyle: UIAlertControllerStyle.alert)
+            
+            let cancelAction = UIAlertAction(title: "OK",
+                                             style: .cancel, handler: nil)
+            
+            alert.addAction(cancelAction)
+            self.present(alert, animated: true,
+                         completion: nil)
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
